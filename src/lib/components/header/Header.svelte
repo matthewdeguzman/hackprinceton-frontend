705bc8b6155d7ef2f297d092a03e38a58d7eb90f
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2024-11-09 15:53:38 -0500

    add home page

---
+++ src/lib/components/header/Header.svelte
@@ -0,0 +1,12 @@
+<header class="border border-solid border-black h-[60px] w-full rounded-2xl flex items-center px-4">
+    <div class="flex items-center grow">
+        <h1>Logo Name</h1>
+    </div>
+
+    <div class="flex gap-4">
+        <a>
+            Log in
+        </a>
+        <a>Sign up</a>
+    </div>
+</header>
